1d017fe3254a35e71d3f17d4bea1183af6df5fbd
smirnovaekaterina <75855500+smirnovaekaterina@users.noreply.github.com>
2021-07-22 11:29:48 +0300

    Add files via upload

--- todolist/app.js
+++ todolist/app.js
@@ -1,86 +1,107 @@
-let userInput = document.getElementById('userInput');
-let buttonEnter = document.getElementById('enter');
-let ul = document.querySelector('ul');
-
-let items = [];
-
-function inputIsNotEmpty() {
-    return userInput.value.length > 0;
-}
-
-function createTodo(text, isDone = false) {
-    items.push(
-        {text: text, isDone: isDone}
-        );
-    userInput.value = '';
-    showItems();
-}
-
-function changeListAfterKeyPress(event) {
-    if (inputIsNotEmpty() && event.which == 13) {
-        createTodo(userInput.value);
+$(function () {
+    const buttonEnter = $('#enter')
+    const userInput = $('#userInput')
+    const ul = $('ul')
+  
+    const items = JSON.parse(localStorage.getItem('todos') || '[]');
+  
+    function saveItems() {
+      localStorage.setItem('todos', JSON.stringify(items))
     }
-}
-
-function changeListAfterButtonPress(event) {
-    if (inputIsNotEmpty()) {
-        createTodo(userInput.value);
+  
+    function addItem(text) {
+      const item = {
+        id: Date.now(),
+        text,
+        done: false,
+      };
+  
+      items.push(item);
+      saveItems();
+  
+      return item;
     }
-}
-
-function showItems() {
-    ul.innerHTML = "";
-    for (var i = 0; i < items.length; i++) {
-        let li = document.createElement('li');
-        li.appendChild(document.createTextNode(items[i].text));
-        if (items[i].isDone) li.classList.toggle('done');
-        ul.appendChild(li);
-
-        li.addEventListener('click', function() {
-            li.classList.toggle('done');
-            collectItems();
-        })
-
-        li.addEventListener('animationend', function() {
-            li.remove();
-            collectItems();
-        });
-
-        let deleteButton = document.createElement('button');
-        deleteButton.appendChild(document.createTextNode('X'));
-        li.appendChild(deleteButton);
-        deleteButton.addEventListener('click', deleteTodoItem);
-
-        function deleteTodoItem() {
-            li.classList.add('delete');
+  
+    function removeItem(id) {
+      const idx = items.findIndex(i => i.id === id)
+  
+      if (idx > -1) {
+        items.splice(idx, 1)
+      }
+  
+      saveItems();
+    }
+  
+    function toggleItem(id) {
+      const idx = items.findIndex(i => i.id === id)
+  
+      if (idx > -1) {
+        items[idx].done = !items[idx].done;
+      }
+  
+      saveItems();
+    }
+  
+    function createItemDOM(item) {
+      const id = item.id;
+      const li = $('<li>')
+      const date = new Date(item.id);
+      const dateString = date.getDate() + "." + (date.getMonth() + 1) + " " + date.getHours() + ":" + date.getMinutes();
+      const text = item.text;
+      li.html("<span>" + text + "</span><span class='d'>" + dateString + "</span>");
+      li.html();
+      ul.append(li)
+  
+      li.click(function() {
+        toggleItem(id)
+        $(this).toggleClass('done')
+      })
+  
+      if (item.done) {
+        li.toggleClass('done')
+      }
+  
+      const deleteButton = $('<button>')
+      deleteButton.html('x')
+      li.append(deleteButton)
+      deleteButton.click(function () {
+        removeItem(id);
+        li.remove();
+      })
+    }
+  
+    function renderItems() {
+      ul.html('');
+  
+      items.forEach((i) => {
+        createItemDOM(i)
+      });
+    }
+  
+    function inputLength() {
+        return userInput.val().length > 0
+    }
+  
+    function createTodo() {
+      const item = addItem(userInput.val())
+      createItemDOM(item);
+      userInput.val('')
+    }
+  
+    function changeListAfterKeypress(event) {
+        if (inputLength() && event.which == 13) {
+          createTodo()
         }
     }
-}
-
-function localStorageClear() {
-    localStorage.setItem("items", "");
-}
-
-function collectItems() {
-    items = [];
-    collectedItems = document.getElementsByTagName('li');
-    for (var i = 0; i < collectedItems.length; i++) {
-        let itemText = collectedItems[i].childNodes.item(0).textContent;
-        let itemIsDone = collectedItems[i].classList.contains('done');
-        items.push({text: itemText, isDone: itemIsDone});
+  
+    function changeListAfterClick() {
+        if (inputLength()) {
+            createTodo()
+        }
     }
-}
-
-userInput.addEventListener('keypress', changeListAfterKeyPress);
-buttonEnter.addEventListener('click', changeListAfterButtonPress);
-
-var retrievedItems = JSON.parse(localStorage.getItem("items"));
-for (var i = 0; i < retrievedItems.length; i++) {
-    items.push({text: retrievedItems[i].text, isDone: retrievedItems[i].isDone});
-}
-showItems();
-
-function OnUnload() {
-    collectItems();
-    localStorage.setItem("items", JSON.stringify(items));
-}
+  
+    userInput.keypress(changeListAfterKeypress)
+    buttonEnter.click(changeListAfterClick);
+  
+    renderItems();
+  });
